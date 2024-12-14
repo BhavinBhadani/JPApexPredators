c372c88c1572152b0d62cfd9119dcfc08fa2c37d
Bhavin B <bhavinbhadani@yahoo.in>
2024-12-14 10:11:33 +0530

    Apex Predator model and list view

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(predators.apexPredators) { predator in
+            HStack {
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 10)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                }
+            }
         }
-        .padding()
     }
 }
 
